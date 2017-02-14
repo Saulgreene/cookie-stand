5dada72e15442bfadfc09138ee3b66e07735fb17
Saul Greene <spdysg@gmail.com>
2017-02-14 14:22:16 -0800

    got table working and generating all required values, still need to work on totals.

--- classtest.js
+++ classtest.js
@@ -1,14 +1,10 @@
 'use strict';
 
-var timeSlot = ['6 am: ', '7 am: ', '8 am: ', '9 am: ', '10 am: ', '11 am: ', '12 pm: ', '1 pm: ', '2 pm: ', '3 pm: ', '4 pm: ', '5 pm: ', '6 pm: ', '7 pm: ', '8 pm: '];
-// var pikeEl = document.getElementById('first-and-pike');
-// var seatacEl = document.getElementById('seatac-airport');
-// var seattleCenterEl = document.getElementById('seattle-center');
-// var capHillEl = document.getElementById('capitol-hill');
-// var alkiEl = document.getElementById('alki');
-// var elements = [pikeEl, seatacEl, seattleCenterEl, capHillEl, alkiEl];
-
-// STORES-------------------------------------------------------------
+//-------------------------VARIABLES-------------------------
+var timeSlot = {
+  label: 'Times: ',
+  times: ['6 am: ', '7 am: ', '8 am: ', '9 am: ', '10 am: ', '11 am: ', '12 pm: ', '1 pm: ', '2 pm: ', '3 pm: ', '4 pm: ', '5 pm: ', '6 pm: ', '7 pm: ']
+};
 var pikePlace = new CookieStore ('First and Pike', 23, 65, 6.3);
 // console.log(pikePlace.displayCookies());
 var seaTac = new CookieStore ('SeaTac', 3, 24, 1.2);
@@ -19,9 +15,26 @@ var capHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
 // console.log(capHill.displayCookies());
 var alki = new CookieStore('Alki', 2, 16, 4.6);
 // console.log(alki.displayCookies());
-
 var tableEl = document.createElement('table');
 var stores = [pikePlace, seaTac, seattleCenter, capHill, alki];
+var total = [];
+//----------------------For Loop to run through the times------------
+
+var rowOneEl = document.createElement('tr');
+var anything = document.createElement('th');
+anything.textContent = timeSlot.label;
+rowOneEl.appendChild(anything);
+
+for (var k = 0; k < timeSlot.times.length ; k++){
+  var timeLabelEl = document.createElement('th');
+  var currentTime = timeSlot.times;
+  timeLabelEl.textContent = currentTime[k];
+  rowOneEl.appendChild(timeLabelEl);
+};
+
+tableEl.appendChild(rowOneEl);
+
+document.body.appendChild(tableEl);
 
 for(var i = 0; i < stores.length; i++){
   var currentStore = stores[i];
@@ -32,45 +45,25 @@ for(var i = 0; i < stores.length; i++){
   nameEl.textContent = currentStore.name;
   rowEl.appendChild(nameEl);
 
-  for(var j = 0; j < timeSlot.length; j++){
+
+  for(var j = 0; j < timeSlot.times.length ; j++){
     var timeEl = document.createElement('td');
     timeEl.textContent = cph();
     rowEl.appendChild(timeEl);
+    total.push(this.cph());
   }
-  // var minCustEl = document.createElement('td');
-  // minCustEl.textContent = currentStore.minCustomers;
-  // rowEl.appendChild(minCustEl);
-  //
-  // var maxCustEl = document.createElement('td');
-  // maxCustEl.textContent = currentStore.maxCustomers;
-  // rowEl.appendChild(maxCustEl);
-  //
-  // var avgCookiesEl = document.createElement('td');
-  // avgCookiesEl.textContent = currentStore.avgCookies;
-  // rowEl.appendChild(avgCookiesEl);
 
   tableEl.appendChild(rowEl);
 };
 
 document.body.appendChild(tableEl);
 
-//---------------------------------------------------------------------
+//------------------------FUNCTIONS----------------------------
 function cph(){
   var avgCookiesPerHour = Math.ceil((Math.floor(Math.random() * ( currentStore.maxCustomers - currentStore.minCustomers + 1) + currentStore.minCustomers)) * currentStore.avgCookies);
   return avgCookiesPerHour;
 };
-function displayCookies(){
-  for (var j = 0; j < timeSlot.length ; j++) {
-    if (j === 0){
-      console.log(this.name);
-    };
-    var userElement = document.createElement('li');
-    var displayHourAndAvg = timeSlot[j] + cph();
-    console.log(displayHourAndAvg);
-    userElement.textContent += displayHourAndAvg;// when i comment out these two lines it logs to the console
-    elements[0].appendChild(userElement);// replace 0 with 'index' and put a for loop in?
-  }
-};
+
 CookieStore.prototype.getAvgCookieCount = function (){
   var range = this.maxCustomers - this.minCustomers;
   return Math.floor(Math.random() * range + this.minCustomers);
@@ -81,22 +74,5 @@ function CookieStore(name, minCustomers, maxCustomers, avgCookies) {
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookies = avgCookies;
-  this.cph = function (){
-    var avgCookiesPerHour = Math.ceil((Math.floor(Math.random() * ( this.max - this.min + 1) + this.min)) * this.avg);
-    return avgCookiesPerHour;
-  };
-  this.displayCookies = function(){
-    for (var j = 0; j < timeSlot.length ; j++) {
-      if (j === 0){
-        console.log(this.name);
-      };
-      var userElement = document.createElement('li');
-      var displayHourAndAvg = timeSlot[j] + this.cph();
-      console.log(displayHourAndAvg);
-      userElement.textContent += displayHourAndAvg;// when i comment out these two lines it logs to the console
-      elements[0].appendChild(userElement);// replace 0 with 'index' and put a for loop in?
-    }
-  };
-};
-
-// console.log(pikePlace.getAvgCookieCount());
+}
+console.log(total);
