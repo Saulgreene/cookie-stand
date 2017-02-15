d2afc19b70dbbe6ef223fdb16f1257a718685920
Saul Greene <spdysg@gmail.com>
2017-02-15 08:56:59 -0800

    very minor, centered the text in the table

--- classtest.js
+++ classtest.js
@@ -1,37 +1,34 @@
 'use strict';
 
-//-------------------------VARIABLES-------------------------
+//-------------------------VARIABLES-------------------------//
 var timeSlot = {
   label: 'Times: ',
   times: ['6am|', '7am|', '8am|', '9am|', '10am|', '11am|', '12pm|', '1pm|', '2pm|', '3pm|', '4pm|', '5pm|', '6pm|', '7pm|'],
-  totalLabel: 'Total|'
-};
+  totalLabel: 'Total|'};
 var pikePlace = new CookieStore ('First and Pike:', 23, 65, 6.3);
-// console.log(pikePlace.displayCookies());
 var seaTac = new CookieStore ('SeaTac:', 3, 24, 1.2);
-// console.log(seaTac.displayCookies());
 var seattleCenter = new CookieStore('Seattle Center:', 11, 38, 3.7);
-// console.log(seattleCenter.displayCookies());
 var capHill = new CookieStore('Capitol Hill:', 20, 38, 2.3);
-// console.log(capHill.displayCookies());
 var alki = new CookieStore('Alki:', 2, 16, 4.6);
-// console.log(alki.displayCookies());
 var tableEl = document.createElement('table');
 var stores = [pikePlace, seaTac, seattleCenter, capHill, alki];
 var total = 0;
-//----------------------For Loop to run through the times------------
 
+//------creating a row item that ends up being labled 'times' on the table-------//
 var rowOneEl = document.createElement('tr');
 var anything = document.createElement('th');
 anything.textContent = timeSlot.label;
 rowOneEl.appendChild(anything);
 
+//-------for loop to run through the time label-----------???????????????????
 for (var k = 0; k < timeSlot.times.length ; k++){
   var timeLabelEl = document.createElement('th');
   var currentTime = timeSlot.times;
   timeLabelEl.textContent = currentTime[k];
   rowOneEl.appendChild(timeLabelEl);
 };
+
+
 var totalHead = document.createElement('th');
 totalHead.textContent = timeSlot.totalLabel;
 rowOneEl.appendChild(totalHead);
@@ -40,6 +37,7 @@ tableEl.appendChild(rowOneEl);
 
 document.body.appendChild(tableEl);
 
+//---------for loop to get through the different stores--------------//
 for(var i = 0; i < stores.length; i++){
   var currentStore = stores[i];
 
@@ -49,7 +47,7 @@ for(var i = 0; i < stores.length; i++){
   nameEl.textContent = currentStore.name;
   rowEl.appendChild(nameEl);
 
-
+//--for loop to produce cph stats for each time slot for this row (it is inside the store loop so it will go 16 times five different times....)
   for(var j = 0; j < timeSlot.times.length ; j++){
     var timeEl = document.createElement('td');
     timeEl.textContent = cph();
@@ -68,16 +66,17 @@ for(var i = 0; i < stores.length; i++){
 document.body.appendChild(tableEl);
 
 //------------------------FUNCTIONS----------------------------
+//---------------function to calculate total cookies per hour for each store------
 function cph(){
   var avgCookiesPerHour = Math.ceil((Math.floor(Math.random() * ( currentStore.maxCustomers - currentStore.minCustomers + 1) + currentStore.minCustomers)) * currentStore.avgCookies);
   return avgCookiesPerHour;
 };
-
+//------------???? what is this again ?????--------------------------------//
 CookieStore.prototype.getAvgCookieCount = function (){
   var range = this.maxCustomers - this.minCustomers;
   return Math.floor(Math.random() * range + this.minCustomers);
 };
-
+// -----------------function to create new cookie store objects.(use of the .this method here)-------//
 function CookieStore(name, minCustomers, maxCustomers, avgCookies) {
   this.name = name;
   this.minCustomers = minCustomers;
