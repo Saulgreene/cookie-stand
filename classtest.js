db0bb4d3812963006d8e076f6047c73f57ddd62c
Saul Greene <spdysg@gmail.com>
2017-02-15 16:40:42 -0800

    added many functions, and am working on making column totals work...

--- classtest.js
+++ classtest.js
@@ -1,4 +1,23 @@
 'use strict';
+var timeSlot = {
+  label: 'Times: ',
+  times: ['6am|', '7am|', '8am|', '9am|', '10am|', '11am|', '12pm|', '1pm|', '2pm|', '3pm|', '4pm|', '5pm|', '6pm|', '7pm|'],
+  totalLabel: 'Total|'
+};
+  //---New store constructors--//
+var pikePlace = new CookieStore ('First and Pike', 23, 65, 6.3);
+var seaTac = new CookieStore ('SeaTac', 3, 24, 1.2);
+var seattleCenter = new CookieStore('Seattle Center', 11, 38, 3.7);
+var capHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
+var alki = new CookieStore('Alki', 2, 16, 4.6);
+  //-- array of the stores for easy access and pulling the info---//
+var stores = [pikePlace, seaTac, seattleCenter, capHill, alki];
+var colTotals = [];
+  //---starts the total at zero so that we can calculate totals within the for loops. resetting to zero allows us to differentiate different rows---//
+var total = 0;
+  //---Creates the table element in the html that we insert into-----//
+var tableEl = document.createElement('table');
+document.body.appendChild(tableEl);
 
 //------------------------FUNCTIONS----------------------------//
 //---------------function to calculate total cookies per hour for each store------
@@ -11,25 +30,27 @@ function CookieStore(name, minCustomers, maxCustomers, avgCookies) {
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookies = avgCookies;
+  this.hourlySales = [];
 }
 CookieStore.prototype.getAvgCookieCount = function (){
   var range = this.maxCustomers - this.minCustomers;
   return Math.floor(Math.random() * range + this.minCustomers);
 };
 CookieStore.prototype.renderRow = function() {
-
   var rowEl = document.createElement('tr');
-
   var nameEl = document.createElement('th');
   nameEl.textContent = this.name + ':';
   rowEl.appendChild(nameEl);
 
 //--for loop to produce cph stats for each time slot for this row (it is inside the store loop so it will go 16 times five different times....)
   for(var j = 0; j < timeSlot.times.length ; j++){
+    var storeRandomCPH = this.getAvgCookieCount();
     var timeEl = document.createElement('td');
-    timeEl.textContent = this.getAvgCookieCount();
+    timeEl.textContent = storeRandomCPH;
     rowEl.appendChild(timeEl);
-    total += this.getAvgCookieCount();
+    total += storeRandomCPH;
+    this.hourlySales.push(storeRandomCPH);
+    console.log(this.hourlySales);
   }
   //---render to dom the total in a column---//
   tableEl.appendChild(rowEl);
@@ -38,6 +59,17 @@ CookieStore.prototype.renderRow = function() {
   rowEl.appendChild(totalEl);
   total = 0;
 };
+function columnTotals(){
+  for (var ii = 0; ii < timeSlot.times.length; ii++){
+    var allStoreHourlyTotal = 0;
+    for(var jj = 0; jj < stores.length; jj++){
+      allStoreHourlyTotal += stores[jj].hourlySales[ii];
+    }
+    colTotals.push(allStoreHourlyTotal);
+  };
+  console.log(colTotals);
+}
+
 console.log('------- EVENT LISTENERS--------');
 //---linking the js to the form tag and id on html---//
 var storeFormEl = document.getElementById('new-store-form');
@@ -62,53 +94,48 @@ function handleSubmit(event){
 
 //-------------------------VARIABLES-------------------------//
 //---timeslot to loop through for rendering to dom and the length of calculations--//
-var timeSlot = {
-  label: 'Times: ',
-  times: ['6am|', '7am|', '8am|', '9am|', '10am|', '11am|', '12pm|', '1pm|', '2pm|', '3pm|', '4pm|', '5pm|', '6pm|', '7pm|'],
-  totalLabel: 'Total|'};
-//---New store constructors--//
-var pikePlace = new CookieStore ('First and Pike', 23, 65, 6.3);
-var seaTac = new CookieStore ('SeaTac', 3, 24, 1.2);
-var seattleCenter = new CookieStore('Seattle Center', 11, 38, 3.7);
-var capHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
-var alki = new CookieStore('Alki', 2, 16, 4.6);
-//-- array of the stores for easy access and pulling the info---//
-var stores = [pikePlace, seaTac, seattleCenter, capHill, alki];
-//---starts the total at zero so that we can calculate totals within the for loops. resetting to zero allows us to differentiate different rows---//
-var total = 0;
-//---Creates the table element in the html that we insert into-----//
-var tableEl = document.createElement('table');
 
 //------creating a row item that ends up being labled 'times' on the table-------//
-var rowOneEl = document.createElement('tr');
-var anything = document.createElement('th');
-anything.textContent = timeSlot.label;
-rowOneEl.appendChild(anything);
+function tableHeader() {
+  var rowOneEl = document.createElement('tr');
+  tableEl.appendChild(rowOneEl);
+  var labelTotalOnTable = document.createElement('th');
+  labelTotalOnTable.textContent = timeSlot.label;
+  rowOneEl.appendChild(labelTotalOnTable);
+  //-----------for loop to run through the time label-----------???????????????????
+  for (var k = 0; k < timeSlot.times.length ; k++){
+    var timeLabelEl = document.createElement('th');
+    var currentTime = timeSlot.times;
+    timeLabelEl.textContent = currentTime[k];
+    rowOneEl.appendChild(timeLabelEl);
+  };
+  var totalHead = document.createElement('th');
+  totalHead.textContent = timeSlot.totalLabel;
+  rowOneEl.appendChild(totalHead);
+}
 
-//-----------for loop to run through the time label-----------???????????????????
-for (var k = 0; k < timeSlot.times.length ; k++){
-  var timeLabelEl = document.createElement('th');
-  var currentTime = timeSlot.times;
-  timeLabelEl.textContent = currentTime[k];
-  rowOneEl.appendChild(timeLabelEl);
-};
 
-//--- creates a row item that is labled total:---//
-var totalHead = document.createElement('th');
-totalHead.textContent = timeSlot.totalLabel;
-rowOneEl.appendChild(totalHead);
-
-tableEl.appendChild(rowOneEl);
-
-document.body.appendChild(tableEl);
 
 //---------for loop to get through the different stores--------------//
-for(var i = 0; i < stores.length; i++){
-  stores[i].renderRow();
+function drawAllStoreRows(){
+  for(var i = 0; i < stores.length; i++){
+    stores[i].renderRow();
+  };
+}
+function drawColumnTotals(){
+  var totalRowEl = document.createElement('tr');
+  totalRowEl.textContent = this.colTotals;
+  rowEl.appendChild(totalRowEl);
+}
+function drawTable(){
+  tableEl.textContent = '';
+  tableHeader();
+  drawAllStoreRows();
+  columnTotals();
+  drawColumnTotals();
+}
+drawTable();
 
-};
-
-document.body.appendChild(tableEl);
 
 //--------NOTES------------//
 // var userElement = document.createElement('h1'); //Step 1 (create element), userElement = <h1></h1> now
